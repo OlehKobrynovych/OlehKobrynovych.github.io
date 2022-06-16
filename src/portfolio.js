1bee5580be78d3b5fe6c0879ecaaa1b45de4e97b
OlehKobrynovych <o.kobrynovych@gmail.com>
2022-06-16 16:31:48 +0300

    update

--- src/portfolio.js
+++ src/portfolio.js
@@ -8,7 +8,7 @@ const header = {
     title: 'ABOUT',
     description:
       'I want to be part of a friendly and professional team where I can develop my skills. I can develop web applications or implement new features / views, maintain, optimize, and debug existing applications. To achieve team results, I am ready to put 100% of my strength.',
-    resume: 'https://drive.google.com/file/d/1G9xqieQaJwO_36peDmyYRtQY8u41B3_N/view?usp=sharing',
+    resume: 'https://drive.google.com/file/d/14Zg9VgvidsyDLsPebeGOPkxRnhxvz3Ab/view?usp=sharing',
     social: {
       linkedin: 'https://www.linkedin.com/in/olehkobrynovych',
       github: 'https://github.com/OlehKobrynovych',
