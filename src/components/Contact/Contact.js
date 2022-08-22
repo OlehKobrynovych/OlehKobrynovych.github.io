a69cdd7904c68c8a844ce013f9da97b8e1c4b041
OlehKobrynovych <o.kobrynovych@gmail.com>
2022-08-22 13:34:16 +0300

    update

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,14 @@
 import { contact } from '../../portfolio'
+import { about } from '../../portfolio'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import WorkIcon from '@mui/icons-material/Work';
+
 import './Contact.css'
 
 const Contact = () => {
+  const { name, title, description, resume, social } = about
+
   if (!contact.email) return null
 
   return (
@@ -12,6 +19,47 @@ const Contact = () => {
           Email me
         </span>
       </a>
+
+      {social.github && (
+        <div className='contact-link-wrap'>
+          <span  className='contact-link__title'>My GitHub: </span> <a
+            title='Go to GitHub'
+            href={social.github}
+            aria-label='github'
+            className='link link--icon'
+          >
+            <GitHubIcon />
+          </a>
+        </div>
+      )}
+
+      {social.linkedin && (
+        <div className='contact-link-wrap'>
+          <span  className='contact-link__title'>My Linkedin: </span>
+          <a
+            title='Go to Linkedin'
+            href={social.linkedin}
+            aria-label='linkedin'
+            className='link link--icon'
+          >
+            <LinkedInIcon />
+          </a>
+        </div>
+      )}
+      
+      {social.upwork && (
+        <div className='contact-link-wrap'>
+          <span  className='contact-link__title'>My Upwork: </span>
+          <a
+            title='Go to Upwork'
+            href={social.upwork}
+            aria-label='upwork'
+            className='link link--icon'
+          >
+            <WorkIcon />
+          </a>
+        </div>
+      )}
     </section>
   )
 }
